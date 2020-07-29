783ded6fd7cf581577a836a84a9139f1572d493d
mvan-eng <merlijnvanengelen@gmail.com>
2020-07-29 14:46:57 +0200

    Changes to check_champions

--- vm/src/check_champions.c
+++ vm/src/check_champions.c
@@ -6,17 +6,22 @@
 /*   By: mvan-eng <mvan-eng@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/15 15:39:40 by mvan-eng      #+#    #+#                 */
-/*   Updated: 2020/07/21 15:31:45 by mvan-eng      ########   odam.nl         */
+/*   Updated: 2020/07/23 15:33:59 by merlijn       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include <unistd.h>
 
-int		check_file_header(char *buff)
+/*
+**	CHECK_FILE_HEADER
+**	compares first 4 bytes for correct magic header 0x00EA83F3
+*/
+
+int		check_file_header(unsigned char *bytecode)
 {
-	if ((unsigned char)buff[0] == 0x00 && (unsigned char)buff[1] == 0xEA &&
-	(unsigned char)buff[2] == 0x83 && (unsigned char)buff[3] == 0xF3)
+	if (bytecode[0] == 0x00 && bytecode[1] == 0xEA && bytecode[2] == 0x83 &&
+	bytecode[3] == 0xF3)
 		return (OK);
 	return (ERROR);
 }
@@ -26,27 +31,40 @@ int		check_file_header(char *buff)
 **	calculates and returns exec_code_size in given bytecode
 */
 
-size_t	get_exec_code_size(char *bytecode)
+size_t	get_exec_code_size(unsigned char *bytecode)
 {
-	size_t size;
+	size_t	size;
+	size_t	offset;
 
-	size = ((unsigned char)bytecode[PROG_NAME_LENGTH + 8] << 24) |
-	((unsigned char)bytecode[PROG_NAME_LENGTH + 9] << 16) |
-	((unsigned char)bytecode[PROG_NAME_LENGTH + 10] << 8) |
-	((unsigned char)bytecode[PROG_NAME_LENGTH + 11]);
+	offset = PROG_NAME_LENGTH + NULL_SIZE + HEADER_SIZE;
+	size = (bytecode[offset] << 24) |
+	(bytecode[offset + 1] << 16) |
+	(bytecode[offset + 2] << 8) |
+	(bytecode[offset + 3]);
 	return (size);
 }
 
-int		check_null(char *bytecode)
+/*
+**	CHECK_NULL
+**	checks if both nulls in .cor file are proper nulls
+**	offset is used to indicate where a null is supposed to start
+*/
+
+int		check_null(unsigned char *bytecode)
 {
-	if (bytecode[PROG_NAME_LENGTH + 4] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + 5] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + 6] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + 7] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + COMMENT_LENGTH + 12] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + COMMENT_LENGTH + 13] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + COMMENT_LENGTH + 14] == 0x00
-	&& bytecode[PROG_NAME_LENGTH + COMMENT_LENGTH + 15] == 0x00)
+	size_t	offset_1;
+	size_t	offset_2;
+
+	offset_1 = PROG_NAME_LENGTH + HEADER_SIZE;
+	offset_2 = PROG_NAME_LENGTH + COMMENT_LENGTH + HEADER_SIZE + 2 * NULL_SIZE;
+	if (bytecode[offset_1] == 0x00
+	&& bytecode[offset_1 + 1] == 0x00
+	&& bytecode[offset_1 + 2] == 0x00
+	&& bytecode[offset_1 + 3] == 0x00
+	&& bytecode[offset_2] == 0x00
+	&& bytecode[offset_2 + 1] == 0x00
+	&& bytecode[offset_2 + 2] == 0x00
+	&& bytecode[offset_2 + 3] == 0x00)
 		return (OK);
 	return (ERROR);
 }
@@ -66,7 +84,7 @@ int		check_file(int fd, t_champion *champion)
 {
 	ft_bzero(&champion->bytecode, CHAMP_FILESIZE + 1);
 	read(fd, &champion->bytecode, CHAMP_FILESIZE);
-	if (check_file_header(&(*champion->bytecode)) == ERROR)
+	if (check_file_header(champion->bytecode) == ERROR)
 		return (ERROR_BAD_HEADER);
 	if (check_null(champion->bytecode) == ERROR)
 		return (ERROR_BAD_NULL);
@@ -79,25 +97,35 @@ int		check_file(int fd, t_champion *champion)
 	return (OK);
 }
 
+/*
+**	CHECK_CHAMPIONS
+**	loops through array of champions to get checked
+*/
+
 int		check_champions(t_champion *champions)
 {
 	int			i;
 	int			ret;
 
 	i = 0;
-	ft_bzero(&champions, sizeof(champions));
-	ret = check_file(champions[i].fd, &champions[i]);
-	if (ret < OK)
+	while (champions[i].file_name != NULL)
 	{
-		if (ret == ERROR_BAD_HEADER)
-			ft_printf("Error: File %s has an invalid header\n", "[filename]");
-		if (ret == ERROR_BAD_SIZE)
-			ft_printf("Error: File %s has too large a code (%d bytes >"
-			" 682 bytes\n", "[filename]", champions[i].exec_code_size);
-		if (ret == ERROR_BAD_NULL)
-			ft_printf("Error: File %s is not properly formatted with nulls\n",
-			"[filename]");
-		exit(ret);
+		ret = check_file(champions[i].fd, &champions[i]);
+		if (ret < OK)
+		{
+			if (ret == ERROR_BAD_HEADER)
+				ft_printf("Error: File %s has an invalid header\n",
+				champions->file_name);
+			if (ret == ERROR_BAD_SIZE)
+				ft_printf("Error: File %s has too large a code (%d bytes >"
+				" 682 bytes\n", champions->file_name,
+				champions[i].exec_code_size);
+			if (ret == ERROR_BAD_NULL)
+				ft_printf("Error: File %s is not properly formatted "
+				"with nulls\n", champions->file_name);
+			exit(ret);
+		}
+		i++;
 	}
 	return (OK);
 }
