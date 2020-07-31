eae0fb02acd2b04a030545e9de4a173a5edb14bb
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2020-07-31 18:53:30 +0200

    chore: add arena printer

--- srcs/vm/arena.c
+++ srcs/vm/arena.c
@@ -6,7 +6,7 @@
 /*   By: wmisiedjan <wmisiedjan@student.codam.nl      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/29 16:16:33 by wmisiedjan    #+#    #+#                 */
-/*   Updated: 2020/07/31 17:19:54 by wmisiedj      ########   odam.nl         */
+/*   Updated: 2020/07/31 18:28:52 by wmisiedj      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ static void        place_champions(t_arena *arena_s)
     i = 0;
     offset = MEM_SIZE / arena_s->champion_count;
 
-    printf_debug("Placing champions in memory...\n");
+    printf_debug("Placing champions in memory...\n\n");
 
     while (i < arena_s->champion_count)
     {
         ft_memcpy(
             arena_s->mem + (i * offset),
-            (const void *)(arena_s->champions[i].champ.exec_code)[i],
-            CHAMP_FILESIZE
+            ((const void *)(arena_s->champions[i].champ.exec_code)),
+            CHAMP_MAX_SIZE
         );
         i++;
     }
@@ -40,5 +40,7 @@ void        start_arena(t_arena *arena_s)
     // First, let's place the champions.
     place_champions(arena_s); 
 
-    // debug_print_map(arena_s);
+    debug_print_map(arena_s);
+
+    printf_debug("\nPlacing cursors...\n");
 }
--- srcs/vm/debug.c
+++ srcs/vm/debug.c
@@ -6,7 +6,7 @@
 /*   By: wmisiedj <wmisiedj@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/29 20:58:11 by wmisiedj      #+#    #+#                 */
-/*   Updated: 2020/07/31 17:38:37 by wmisiedj      ########   odam.nl         */
+/*   Updated: 2020/07/31 18:29:41 by wmisiedj      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,49 @@ void        debug_print_hex(unsigned char *str, int n)
     ft_printf("\n");
 }
 
+void	print_memory(const void *addr, size_t size)
+{
+	size_t			i;
+	size_t 			j;
+	unsigned char	*p;
+	char 			*str;
+    size_t             width;
+    
+    width = 64;
+	str = "0123456789abcdef";
+	p = (unsigned char *)addr;
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < width && i + j < size)
+		{
+            printf_debug("%02hhX", p[i + j]);
+			if (j % 2)
+				printf_debug(" ");
+			j += 1;
+		}
+		while (j < width)
+		{
+			printf_debug(" ");
+			if (j % 2)
+				printf_debug(" ");
+			j++;
+		}
+		j = 0;
+		while (j < width && i + j < size)
+		{
+            printf_debug("%c", ft_isprint(p[i + j]) ? p[i + j] : '.');
+			j += 1;
+		}
+		printf_debug("\n");
+		i += width;
+	}
+}
+
 void        debug_print_map(t_arena *arena)
 {
-    int i;
-
-    i = 0;
-    printf_debug("Current memory:\n");
-    while (i < MEM_SIZE)
-    {
-        printf_debug("%.2x ", arena->mem[i]);
-        i++;
-    }
+    print_memory(arena->mem, MEM_SIZE);
 }
 
 int		printf_debug(const char *format, ...)
