ecb048e89e4ecc742391f9cad682a26962de048b
Floris Fredrikze <floris@noxbox.nl>
2020-08-27 14:46:48 +0200

    feat: start on meta parser

---
+++ srcs/asm/parser/metaparse.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   metaparse.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: floris <ffredrik@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/27 13:01:20 by floris        #+#    #+#                 */
+/*   Created: 2020/08/27 13:01:20 by floris        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef METAPARSE_H
+#define METAPARSE_H
+
+#include "asm.h"
+
+t_ret	get_meta_from_file(char *file, t_tksave tokens[2], t_error *error);
+
+#endif
