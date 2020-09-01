b83d1834324e51eefdf7bb40377ed3d797f6d669
Floris Fredrikze <floris@noxbox.nl>
2020-09-01 10:13:24 +0200

    feature: add ft_lstlen to libft

--- srcs/lib/libft/libft.h
+++ srcs/lib/libft/libft.h
@@ -134,6 +134,7 @@ void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void(*f)(t_list *elem));
 void			ft_lstrev(t_list **lst);
 t_list			*ft_lstmap(t_list *lst, t_list*(*f)(t_list *elem));
+size_t			ft_lstlen(t_list *lst);
 
 # pragma mark - Dictionary
 
