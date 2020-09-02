104b46261545daced30cbc15fc0bccd1fc1183c4
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2020-09-02 13:04:08 +0200

    chore/norm fixes in utils.c

--- srcs/vm/utils.c
+++ srcs/vm/utils.c
@@ -6,17 +6,16 @@
 /*   By: wmisiedj <wmisiedj@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/30 19:22:32 by wmisiedj      #+#    #+#                 */
-/*   Updated: 2020/08/31 18:52:16 by mvan-eng      ########   odam.nl         */
+/*   Updated: 2020/09/02 12:58:56 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "vm.h"
 
-uint32_t rev_bytes_32(uint32_t value)
+uint32_t	rev_bytes_32(uint32_t value)
 {
-     return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
-         (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+	return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+	(value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
 }
 
 // static int  char_base(int c, int base)
@@ -34,24 +33,23 @@ uint32_t rev_bytes_32(uint32_t value)
 // 	return (-1);
 // }
 
-int     ft_strntoi(unsigned char *str, int n)
+int			ft_strntoi(unsigned char *str, int n)
 {
-    unsigned char    *start;
-    int     total;
-    int     i;
+	unsigned char	*start;
+	int				total;
+	int				i;
 
-    i = 0;
-    total = 0;
-    start = str;
-    ft_printf("DATA: '%s'\n", str);
-    while (i < n)
-    {
-        ft_printf("Checking: (%d) %d\n", i, start[i]);
-        total *= 2;
-        if (start[i] == '1')
-            total += 1;
-        i++;
-    }
-
-    return (total);
+	i = 0;
+	total = 0;
+	start = str;
+	ft_printf("DATA: '%s'\n", str);
+	while (i < n)
+	{
+		ft_printf("Checking: (%d) %d\n", i, start[i]);
+		total *= 2;
+		if (start[i] == '1')
+			total += 1;
+		i++;
+	}
+	return (total);
 }
