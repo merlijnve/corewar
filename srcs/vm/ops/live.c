6318994266d602ed9c327aa49b926eecd0203bb9
Joris Boer <j.boer13@gmail.com>
2020-09-10 15:59:52 +0200

    live testing needed

--- srcs/vm/ops/live.c
+++ srcs/vm/ops/live.c
@@ -6,25 +6,37 @@
 /*   By: floris <ffredrik@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/06 10:00:00 by floris        #+#    #+#                 */
-/*   Updated: 2020/09/10 13:17:21 by joris         ########   odam.nl         */
+/*   Updated: 2020/09/10 15:55:09 by joris         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
+static t_champion	*find_player_id(t_arena *arena, int id)
+{
+	int		i;
+
+	i = 0;
+	while (i < MAX_CHAMPS)
+	{
+		if (arena->champions[i].id == id)
+			return (&arena->champions[i]);
+		i++;
+	}
+	return (NULL);
+}
+
 // TODO: Check if this is absolutely everything
 void	inst_live(t_arena *arena, t_cursor *cursor)
 {
-	int champ;
-	int	t_dir_size;
+	t_champion *player;
 
-	t_dir_size = 0; //Need fix, get_arguments.c is deleted
 	arena->live_count += 1;
-	champ = dir_arg(arena->mem, cursor->pos, t_dir_size);
-	champ = champ * -1;
-	if (champ < MAX_CHAMPS && champ > -1 && arena->champions[champ - 1])
+	player = find_player_id(arena, cursor->registries[0]);
+
+	if (player < MAX_CHAMPS && player > -1)
 	{
-		// TODO: find player for id
+
 		arena->winner = &arena->champions[0];
 	}
 	cursor->last_alive = arena->cycle_count;
