1c17365cf14be3c937e2c821d04f445716fc886d
Floris Fredrikze <floris@noxbox.nl>
2020-09-10 15:22:19 +0200

    fix: fix loading of args in add and sub

--- srcs/vm/ops/add.c
+++ srcs/vm/ops/add.c
@@ -19,8 +19,16 @@ void		inst_add(t_arena *arena, t_cursor *cursor)
 
 	if (arena == NULL || cursor == NULL)
 		return;
-	args[0] = cursor->registries[cursor->args[0].value - 1];
-	args[1] = cursor->registries[cursor->args[1].value - 1];
+
+	if (cursor->args[0].type == kTReg)
+		args[0] = cursor->registries[cursor->args[0].value - 1];
+	else
+		args[0] = cursor->args[0].value;
+	if (cursor->args[1].type == kTReg)
+		args[1] = cursor->registries[cursor->args[1].value - 1];
+	else
+		args[1] = cursor->args[1].value;
+
 	sum = args[0] + args[1];
 	cursor->registries[cursor->args[2].value - 1] = sum;
 	if (sum == 0)
