8181334d1da1f20134497c3ad44f688fae2d09aa
Joris Boer <j.boer13@gmail.com>
2020-09-10 16:01:53 +0200

    live

--- srcs/vm/ops/live.c
+++ srcs/vm/ops/live.c
@@ -6,7 +6,7 @@
 /*   By: floris <ffredrik@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/06 10:00:00 by floris        #+#    #+#                 */
-/*   Updated: 2020/09/10 15:55:09 by joris         ########   odam.nl         */
+/*   Updated: 2020/09/10 16:00:44 by joris         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,13 @@ static t_champion	*find_player_id(t_arena *arena, int id)
 	return (NULL);
 }
 
-// TODO: Check if this is absolutely everything
 void	inst_live(t_arena *arena, t_cursor *cursor)
 {
-	t_champion *player;
-
 	arena->live_count += 1;
-	player = find_player_id(arena, cursor->registries[0]);
-
-	if (player < MAX_CHAMPS && player > -1)
+	if (cursor->args[0].value == cursor->registries[0])
 	{
-
-		arena->winner = &arena->champions[0];
+		arena->winner = find_player_id(arena, cursor->registries[0] * -1);
 	}
 	cursor->last_alive = arena->cycle_count;
-
 	return;
 }
-
