1c186246940f05b9e5b539fcc81bb44243aba877
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2020-09-13 14:33:19 +0200

    Added if for aff, is_valid_enbyte improvement

--- srcs/vm/op_arch_checker.c
+++ srcs/vm/op_arch_checker.c
@@ -6,7 +6,7 @@
 /*   By: floris <ffredrik@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/06 13:01:20 by floris        #+#    #+#                 */
-/*   Updated: 2020/09/09 21:12:48 by merlijn       ########   odam.nl         */
+/*   Updated: 2020/09/13 14:32:10 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,12 @@ bool is_valid_enbyte(t_inst inst, t_enbyte enbyte)
 		ret = is_valid_arg(inst, enbyte.arg2, 2);
 	if (ret)
 		ret = is_valid_arg(inst, enbyte.arg3, 3);
+	if (ret)
+	{
+		if (enbyte.arg4 == 0)
+			ret = true;
+		else
+			ret = false;
+	}
 	return (ret);
 }
--- srcs/vm/ops/aff.c
+++ srcs/vm/ops/aff.c
@@ -6,7 +6,7 @@
 /*   By: merlijn <merlijn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/19 18:05:23 by merlijn       #+#    #+#                 */
-/*   Updated: 2020/09/12 01:14:44 by mvan-eng      ########   odam.nl         */
+/*   Updated: 2020/09/12 19:45:29 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	inst_aff(t_arena *arena, t_cursor *cursor)
 {
 	if (arena == NULL || cursor == NULL)
 		return;
-	ft_printf("%c", (char)cursor->args[0].value);
+	if (arena->dump_flag == -1)
+		ft_printf("%c", (char)cursor->args[0].value);
 }
