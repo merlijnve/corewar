9344d29548baf15d3e86ac23c1a0496bc80bba66
Joris Boer <j.boer13@gmail.com>
2020-09-14 16:47:24 +0200

    fix/function_lines: enbyte

--- srcs/vm/enbytes.c
+++ srcs/vm/enbytes.c
@@ -6,7 +6,7 @@
 /*   By: mvan-eng <mvan-eng@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/09 20:48:30 by mvan-eng      #+#    #+#                 */
-/*   Updated: 2020/09/14 15:52:05 by joris         ########   odam.nl         */
+/*   Updated: 2020/09/14 16:24:00 by joris         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void		reverse_eb(t_enbyte *eb)
 t_enbyte	*get_enbyte(t_arena *arena, long pos)
 {
 	t_enbyte *eb;
-	
+
 	eb = (t_enbyte *)&arena->mem[get_pos(pos, 1)];
 	return (eb);
 }
