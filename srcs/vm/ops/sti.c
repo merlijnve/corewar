cd599b9ac95c314ba6544319d6aa1e1ead1c7919
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2020-09-14 22:29:11 +0200

    chore: cleanup sti readability

--- srcs/vm/ops/sti.c
+++ srcs/vm/ops/sti.c
@@ -6,7 +6,7 @@
 /*   By: merlijn <merlijn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/18 21:42:27 by merlijn       #+#    #+#                 */
-/*   Updated: 2020/09/14 22:11:33 by wmisiedj      ########   odam.nl         */
+/*   Updated: 2020/09/14 22:26:49 by wmisiedj      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void		inst_sti(t_arena *arena, t_cursor *cursor)
 {
 	if (arena == NULL || cursor == NULL)
 		return ;
+	if (cursor->args[0].type == kTReg)
+		cursor->args[0].value = cursor->registries[cursor->args[0].value - 1];
 	if (cursor->args[1].type == kTReg)
 		cursor->args[1].value = cursor->registries[cursor->args[1].value - 1];
 	if (cursor->args[2].type == kTReg)
@@ -28,5 +30,5 @@ void		inst_sti(t_arena *arena, t_cursor *cursor)
 	write_4_bytes(arena->mem,
 		get_pos(cursor->pos,
 			(cursor->args[1].value + cursor->args[2].value) % IDX_MOD),
-		cursor->registries[cursor->args[0].value - 1]);
+		cursor->args[0].value);
 }
